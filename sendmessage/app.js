45585624cfb14b1f21e7b0420b29cbad582960be
Sajan Gurung <sjnkon@gmail.com>
2019-08-04 18:04:46 +1000

    save input bid in ddb and update maxbid

--- sendmessage/app.js
+++ sendmessage/app.js
@@ -7,6 +7,34 @@ const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 const { TABLE_NAME } = process.env;
 
+const update = async (connectionId, bid) => {
+
+  const params = {
+    TableName: TABLE_NAME,
+    Key: { connectionId: connectionId },
+    UpdateExpression: 'set #bids = list_append(if_not_exists(#bids, :empty_list), :bid), #currentMaxBid = :maxBid',
+    ConditionExpression: "currentMaxBid < :maxBid",
+    ExpressionAttributeNames: {
+      '#bids': 'bid',
+      '#currentMaxBid': 'currentMaxBid'
+    },
+    ExpressionAttributeValues: {
+      ':bid': [bid],
+      ':maxBid': bid,
+      ':empty_list': []
+    },
+    ReturnValues: 'UPDATED_NEW'
+  };
+
+  console.log('Params', params);
+  try {
+    const { Attributes } = await ddb.update(params).promise();
+    return Attributes;
+  } catch (e) {
+    console.log('Error updating db:', e.stack);
+  }
+}
+
 exports.handler = async (event, context) => {
   let connectionData;
 
@@ -23,8 +51,36 @@ exports.handler = async (event, context) => {
 
   const postData = JSON.parse(event.body).data;
 
+  if (isNaN(postData)) {
+    return { statusCode: 400, body: 'Please provide a number to bid' };
+  }
+
   const postCalls = connectionData.Items.map(async ({ connectionId }) => {
-    console.log('ConnectionId', connectionId);
+    const bid = parseFloat(postData);
+
+    const params = {
+      TableName: TABLE_NAME,
+      Key: { connectionId: connectionId },
+      UpdateExpression: 'set #bids = list_append(if_not_exists(#bids, :empty_list), :bid), #currentMaxBid = :maxBid',
+      ConditionExpression: "currentMaxBid < :maxBid",
+      ExpressionAttributeNames: {
+        '#bids': 'bid',
+        '#currentMaxBid': 'currentMaxBid'
+      },
+      ExpressionAttributeValues: {
+        ':bid': [bid],
+        ':maxBid': bid,
+        ':empty_list': [],
+      },
+      ReturnValues: 'UPDATED_NEW'
+    };
+
+    try {
+      await ddb.update(params).promise();
+    } catch (e) {
+      console.log('Error updating db:', e.stack);
+    }
+
     try {
       await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
     } catch (e) {
